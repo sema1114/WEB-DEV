974b3c11eb6e2259356a9de358fd3bae92a61c3c
sema <semabayri00@gmail.com>
2021-04-06 00:04:05 +0200

    Add Item

--- ANGULAR/todoApp/src/app/app.component.ts
+++ ANGULAR/todoApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +17,13 @@ export class AppComponent {
      return this.model.items.filter(item=>!item.action);
      //item action olmyanları al
    }
+
+   addItem(value){
+   if(value!=""){
+   this.model.items.push(new TodoItem(value,false));
+   }
+   }
+
+
+
 }
