c15c3d401daf390bfd47ecc0e12ba23961916032
sema <semabayri00@gmail.com>
2021-04-10 18:50:58 +0200

    add

---
+++ ANGULAR/MovieApp/src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector :'movies', //<movies></movies> eğer '.movies' olursa <div class='movies'> '#movies' olursa <div id='movies'>
+  template :'<h2>Movies</h2>'
+})
+
+export class Movies{
+
+}
