46aa7091f331b622c18f5d315125e21e26f6af3f
sema <semabayri00@gmail.com>
2021-04-10 21:02:36 +0200

    Lesson 3 :Component Styles

--- ANGULAR/MovieApp/src/app/movies/movies.component.ts
+++ ANGULAR/MovieApp/src/app/movies/movies.component.ts
@@ -3,6 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector :'movies', //<movies></movies> eğer '.movies' olursa <div class='movies'> '#movies' olursa <div id='movies'>
   templateUrl:'movies.component.html',
+  styles:[`
+  h2{
+    color:blue;
+  }
+  `]
 })
 
 export class MoviesComponent{
