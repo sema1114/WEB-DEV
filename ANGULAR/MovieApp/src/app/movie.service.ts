2b0307d3070b401af039d5862be5bf4672e00d6c
sema <semabayri00@gmail.com>
2021-04-13 01:51:31 +0200

    Lesson 8: orking With Services

---
+++ ANGULAR/MovieApp/src/app/movie.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Movie } from './movie';
+import {Movies} from './movie.datasource';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+
+
+
+getMovie() : Movie[]{
+
+  return Movies;
+
+}
+}
--- ANGULAR/MovieApp/src/app/movies/movies.component.ts
+++ ANGULAR/MovieApp/src/app/movies/movies.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { Movie } from "../movie";
-import {Movies} from '../movie.datasource'
+import {MovieService} from '../movie.service';
 
 @Component({
   selector :'movies', //<movies></movies> eğer '.movies' olursa <div class='movies'> '#movies' olursa <div id='movies'>
@@ -12,11 +12,24 @@ export class MoviesComponent{
 
   title='Movie List';
 
-  movies=Movies;
+  movies:Movie[];
+
+  constructor( private movieService:MovieService){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+   this.getMovies();
+  }
+
   selectedMovie:Movie;
 
  onSelect(movie:Movie):void{
 this.selectedMovie=movie;
+ }
+
+ getMovies() :void{
+   this.movies=this.movieService.getMovie();
  }
 
 }
