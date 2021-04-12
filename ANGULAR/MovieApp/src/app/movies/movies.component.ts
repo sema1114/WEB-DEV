96aa75e5cfe60128fbc496dac41be52a92492111
sema <semabayri00@gmail.com>
2021-04-12 23:53:28 +0200

    lesson 4: Working with template

--- ANGULAR/MovieApp/src/app/movies/movies.component.ts
+++ ANGULAR/MovieApp/src/app/movies/movies.component.ts
@@ -1,15 +1,22 @@
 import { Component } from "@angular/core";
+import { Movie } from "../movie";
 
 @Component({
   selector :'movies', //<movies></movies> eğer '.movies' olursa <div class='movies'> '#movies' olursa <div id='movies'>
-  templateUrl:'movies.component.html',
-  styles:[`
-  h2{
-    color:blue;
-  }
-  `]
+  templateUrl:'movies.component.html'
+
 })
 
 export class MoviesComponent{
 
+  title='Movie List'
+
+  movie : Movie={
+    id:1,
+    name:'Movie Name'
+  }
+
+  getTitle(){
+    return this.title;
+  }
 }
