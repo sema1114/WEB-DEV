f57f82f91efa5290db27a1986cdc9edda5780018
sema <semabayri00@gmail.com>
2021-04-15 11:38:24 +0200

    6- Style Binding

--- ANGULAR/example/src/app/product.component.ts
+++ ANGULAR/example/src/app/product.component.ts
@@ -10,34 +10,16 @@ import { ProductRepository } from "./repository.model";
 export class ProductComponent{
 
 model:ProductRepository=new ProductRepository();
-disabled=true;
 
-getClasses(id:number):string{
-  let product=this.model.getProductsById(id);
-  return (product.price <=1000 ? "bg-info" : "bg-secondary")+ " m-2 p-2 text-white";
+
+onSubmit($event){
+  $event.stopPropagation();
+console.log("Button was clicked");
+console.log($event);
 }
 
-getClassMap(id:number) :Object{
-
-  let product =this.model.getProductsById(id);
-
-  return {
-    "bg-info" :product.price<=1000,
-    "bg-secondary" :product.price>1000,
-    "text-center text-white" : product.name == "Samsung S6",
-
-
-  }
+onDivClicked(){
+console.log('div was clicked');
 }
- color :string =this.model.getProductsById(1).price<=1000 ? "green":"red";
- fontSize :string="25px";
-
- getStyles(id:number){
-   let product=this.model.getProductsById(id);
-   return{
-     fontSize:"25px",
-     color:product.price<=1000 ? "green" :"red"
-   }
- }
 
 }
