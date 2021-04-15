dc98539f10387aff9f427bbf86916698fba9e436
sema <semabayri00@gmail.com>
2021-04-15 19:45:40 +0200

    Custom Pipe

--- ANGULAR/example/src/app/app.module.ts
+++ ANGULAR/example/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProductComponent } from './product.component';
 import {FormsModule} from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
-  ProductComponent
+  ProductComponent,
+  SummaryPipe
   ],
   imports: [
     BrowserModule,
