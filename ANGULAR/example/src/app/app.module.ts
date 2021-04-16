c5e305f1dba024918c10b4bf8b11fbf6a3e5975b
sema <semabayri00@gmail.com>
2021-04-16 19:52:47 +0200

    !!Önemli unutulan değeri girme

--- ANGULAR/example/src/app/app.module.ts
+++ ANGULAR/example/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { ProductComponent } from './product.component';
 import {FormsModule} from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
+import { InputEmailDirective } from './input-email.directive';
 
 @NgModule({
   declarations: [
   ProductComponent,
-  SummaryPipe
+  SummaryPipe,
+  InputEmailDirective
   ],
   imports: [
     BrowserModule,
