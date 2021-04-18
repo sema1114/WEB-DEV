eb2ba5474db98eca3cbff4a1f1370f60474aa623
sema <semabayri00@gmail.com>
2021-04-18 14:26:32 +0200

    16-List

--- ANGULAR/example/src/app/app.module.ts
+++ ANGULAR/example/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { ProductComponent } from './product.component';
 import {FormsModule} from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
 import { InputEmailDirective } from './input-email.directive';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
 
 @NgModule({
   declarations: [
   ProductComponent,
   SummaryPipe,
-  InputEmailDirective
+  InputEmailDirective,
+  AdminProductsComponent
   ],
   imports: [
     BrowserModule,
