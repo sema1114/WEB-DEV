02ea6485e2f67302f1e82de1c6b5f7adf000fc5c
sema <semabayri00@gmail.com>
2021-04-24 17:53:44 +0200

    Working with Form Control

--- ANGULAR/REACTFORM2/src/app/product.component.ts
+++ ANGULAR/REACTFORM2/src/app/product.component.ts
@@ -11,8 +11,11 @@ export class ProductComponent {
 
 name =new FormControl('Samsung S6');
 description = new FormControl('İyi telefon');
-price =new FormControl(1000);
+price =new FormControl('1000');
 imageUrl=new FormControl('1.jpg');
 
+updateName(){
+  this.name.setValue('Samsung S10')
+}
 
 }
