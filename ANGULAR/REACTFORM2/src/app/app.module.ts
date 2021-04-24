4a4b33d6ab40155fa1db058afd4568cdea584899
sema <semabayri00@gmail.com>
2021-04-24 17:18:55 +0200

    Added some html

--- ANGULAR/REACTFORM2/src/app/app.module.ts
+++ ANGULAR/REACTFORM2/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ProductComponent } from './product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [ProductComponent]
